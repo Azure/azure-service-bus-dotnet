6d364eb357a996b809e0e6f5a46d5a8efded8ef8
Vinay Suryanarayana <viny.surya@gmail.com>
2017-02-08 13:04:43 -0800

    Add subscription Client based tests.

--- test/Microsoft.Azure.ServiceBus.UnitTests/OnMessageQueueTests.cs
+++ test/Microsoft.Azure.ServiceBus.UnitTests/OnMessageQueueTests.cs
@@ -5,12 +5,10 @@ namespace Microsoft.Azure.ServiceBus.UnitTests
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Threading.Tasks;
-    using Microsoft.Azure.Amqp.Framing;
     using Xunit;
 
-    public class OnMessageTests
+    public class OnMessageQueueTests : SenderReceiverClientTestBase
     {
         public static IEnumerable<object> TestPermutations => new object[]
         {
@@ -53,35 +51,7 @@ namespace Microsoft.Azure.ServiceBus.UnitTests
                 mode);
             try
             {
-                int count = 0;
-                await TestUtility.SendMessagesAsync(queueClient.InnerSender, messageCount);
-                queueClient.OnMessageAsync(
-                async (message, token) =>
-                {
-                    Console.WriteLine($"Received message: SequenceNumber: {message.SequenceNumber}");
-                    count++;
-                    if (mode == ReceiveMode.PeekLock && !autoComplete)
-                    {
-                        await message.CompleteAsync();
-                    }
-                    await Task.FromResult(0);
-                },
-                new OnMessageOptions() { MaxConcurrentCalls = maxConcurrentCalls });
-
-                // Wait for the OnMessage Tasks to finish
-                Stopwatch stopwatch = Stopwatch.StartNew();
-                while (stopwatch.Elapsed.TotalSeconds <= 60)
-                {
-                    if (count == messageCount)
-                    {
-                        Console.WriteLine($"All '{messageCount}' messages Received.");
-                        break;
-                    }
-                    else
-                    {
-                        await Task.Delay(TimeSpan.FromSeconds(5));
-                    }
-                }
+                await this.OnMessageAsyncTestCase(queueClient.InnerSender, queueClient.InnerReceiver, maxConcurrentCalls, autoComplete, messageCount);
             }
             finally
             {
