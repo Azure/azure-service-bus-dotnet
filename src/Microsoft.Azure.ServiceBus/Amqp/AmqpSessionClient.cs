fc902d0ef092768835089735148779603a77797a
jtaubensee <jotaub@microsoft.com>
2017-05-01 16:52:13 -0700

    Renamed AmqpSessionClient to MessageSession, and refactored accordingly

--- src/Microsoft.Azure.ServiceBus/Amqp/AmqpSessionClient.cs
+++ src/Microsoft.Azure.ServiceBus/Amqp/AmqpSessionClient.cs
@@ -100,7 +100,7 @@ namespace Microsoft.Azure.ServiceBus.Amqp
                 throw AmqpExceptionHelper.GetClientException(exception);
             }
 
-            MessageSession session = new AmqpMessageSession(receiver.SessionId, receiver.LockedUntilUtc, receiver, this.RetryPolicy);
+            MessageSession session = new MessageSession(receiver.SessionId, receiver.LockedUntilUtc, receiver, this.RetryPolicy);
 
             MessagingEventSource.Log.AmqpSessionClientAcceptMessageSessionStop(
                 this.ClientId,
