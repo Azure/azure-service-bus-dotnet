00012ba703d03f1a21832266c095c81f1fb83849
Sean Feldman <feldman.sean@gmail.com>
2017-06-05 21:28:52 -0600

    making ExceptionReceivedEventArgsAction public

--- src/Microsoft.Azure.ServiceBus/ExceptionReceivedEventArgsAction.cs
+++ src/Microsoft.Azure.ServiceBus/ExceptionReceivedEventArgsAction.cs
@@ -1,13 +1,29 @@
 ﻿namespace Microsoft.Azure.ServiceBus
 {
-    static class ExceptionReceivedEventArgsAction
+    using Microsoft.Azure.ServiceBus.Primitives;
+
+    /// <summary>Action taking place when <see cref="ExceptionReceivedEventArgs"/> is raised for a processed message.</summary>
+    public static class ExceptionReceivedEventArgsAction
     {
+        /// <summary>Completion operation</summary>
         public const string Complete = "Complete";
+        
+        /// <summary>Abandon operation</summary>
         public const string Abandon = "Abandon";
+        
+        /// <summary>User message handler invocation</summary>
         public const string UserCallback = "UserCallback";
+        
+        /// <summary>Receive operation</summary>
         public const string Receive = "Receive";
+
+        /// <summary>Message lock renewal operation</summary>
         public const string RenewLock = "RenewLock";
+
+        /// <summary>Session start operation</summary>
         public const string AcceptMessageSession = "AcceptMessageSession";
+
+        /// <summary>Session close operation</summary>
         public const string CloseMessageSession = "CloseMessageSession";
     }
 }
