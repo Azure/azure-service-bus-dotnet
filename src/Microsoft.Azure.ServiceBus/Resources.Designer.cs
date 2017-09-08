a673a1e966b6d790021a8cb085022c7109ba0deb
Sean Feldman <feldman.sean@gmail.com>
2017-09-07 21:06:43 -0600

    non-compliant with .net standard code

--- src/Microsoft.Azure.ServiceBus/Resources.Designer.cs
+++ src/Microsoft.Azure.ServiceBus/Resources.Designer.cs
@@ -8,9 +8,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.Reflection;
+
 namespace Microsoft.Azure.ServiceBus {
-    using System;
-    
     
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -39,7 +39,7 @@ namespace Microsoft.Azure.ServiceBus {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Azure.ServiceBus.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Azure.ServiceBus.Resources", typeof(Resources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
