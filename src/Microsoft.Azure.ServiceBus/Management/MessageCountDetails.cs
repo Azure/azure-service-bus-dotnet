dc32047f07f7f51ae8de76623184cb7dc99fe304
Neeraj Makam <neeraj.makam@gmail.com>
2018-05-14 17:16:45 -0700

    queuedescription

---
+++ src/Microsoft.Azure.ServiceBus/Management/MessageCountDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Azure.ServiceBus.Management
+{
+    public class MessageCountDetails
+    {
+        public long ActiveMessageCount { get; set; }
+
+        public long DeadLetterMessageCount { get; set; }
+
+        public long ScheduledMessageCount { get; set; }
+
+        public long TransferMessageCount { get; set; }
+
+        public long TransferDeadLetterMessageCount { get; set; }
+    }
+}
