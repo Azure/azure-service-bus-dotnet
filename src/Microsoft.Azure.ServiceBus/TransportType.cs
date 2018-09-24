096de7972173ea408ad71afdeec79ab2cb481910
Neeraj Makam <neeraj.makam@microsoft.com>
2018-09-24 11:28:02 -0700

    Updating xml doc - nit

--- src/Microsoft.Azure.ServiceBus/TransportType.cs
+++ src/Microsoft.Azure.ServiceBus/TransportType.cs
@@ -18,7 +18,7 @@ namespace Microsoft.Azure.ServiceBus
         /// Uses AMQP over WebSockets
         /// </summary>
         /// <remarks>This runs on port 443 with wss URI scheme. This could be used in scenarios where traffic to port 5671 is blocked. 
-        /// To setup a proxy connection, please configure system default proxy. Proxy currently is supported only in net451+ and .netcore 2.1+ framework.</remarks>
+        /// To setup a proxy connection, please configure system default proxy. Proxy currently is supported only in  .NET 4.5.1 and higher or .NET Core 2.1 and higher.</remarks>
         AmqpWebSockets = 1
     }
 }
