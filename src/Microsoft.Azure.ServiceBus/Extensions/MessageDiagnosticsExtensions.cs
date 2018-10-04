8f98ee262a456eb289866916c63c53b8d8c03762
Sean Feldman <feldman.sean@gmail.com>
2018-10-04 09:06:23 -0600

    Move extension method into appropreate class

--- src/Microsoft.Azure.ServiceBus/Extensions/MessageDiagnosticsExtensions.cs
+++ src/Microsoft.Azure.ServiceBus/Extensions/MessageDiagnosticsExtensions.cs
@@ -150,14 +150,5 @@ namespace Microsoft.Azure.ServiceBus.Diagnostics
             }
             return false;
         }
-
-
-        internal static void VerifyMessageIsNotPreviouslyReceived(this Message message)
-        {
-            if (message.SystemProperties.IsLockTokenSet)
-            {
-                throw Fx.Exception.Argument(nameof(message), "Cannot send a message that was already received.");
-            }
-        }
     }
 }
---
+++ src/Microsoft.Azure.ServiceBus/Extensions/MessageExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Azure.ServiceBus
+{
+    using Microsoft.Azure.ServiceBus.Primitives;
+
+    internal static class MessageExtensions
+    {
+        public static void VerifyMessageIsNotPreviouslyReceived(this Message message)
+        {
+            if (message.SystemProperties.IsLockTokenSet)
+            {
+                throw Fx.Exception.Argument(nameof(message), "Cannot send a message that was already received.");
+            }
+        }
+    }
+}
